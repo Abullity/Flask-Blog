43962d42692fbf4901ad35e879848f2bb72e5ddf
divi <goldenpelmeni@gmail.com>
2021-04-28 16:15:23 +0400

    Added home and about pages

---
+++ WebData.py
@@ -0,0 +1,26 @@
+posts = [
+    {
+        "name": "Carson",
+        "username": "CallMeCarsonYT",
+        "date": "Jan 5",
+        "content": "if you spell your name wrong on an IQ test does your score get knocked down"
+    },
+    {
+        "name": "Carson",
+        "username": "CallMeCarsonYT",
+        "date": "Jan 3",
+        "content": "yeah it's cool and all that it's 2021 now, but wait until you realize that 2020 was LAST YEAR!"
+    },
+    {
+        "name": "Carson",
+        "username": "CallMeCarsonYT",
+        "date": "Jan 1",
+        "content": "can't wait for covid to go away tomorrow"
+    },
+    {
+        "name": "Carson",
+        "username": "CallMeCarsonYT",
+        "date": "Dec 30, 2020",
+        "content": "you're tellin me a shrimp fried this rice"
+    }
+]
