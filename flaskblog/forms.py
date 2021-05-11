bd049149e4ca6a26246a94e8ee1e5ad99f772598
divi <goldenpelmeni@gmail.com>
2021-05-11 11:48:18 +0400

    Real data is used for posts, posts can be update and deleted

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
+
 
 
 class RegistrationForm(FlaskForm):
@@ -31,3 +34,27 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
     
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=50)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+    submit = SubmitField('Update')
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("The username is taken, try something else!")
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("The email is taken, try something else!")
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    post_content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField("Post")                
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,14 +1,17 @@
+import os
+import secrets
+from PIL import Image
 from flaskblog import app, bcrypt, db
-from flask import render_template, url_for, flash, redirect, request
-from flaskblog.forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect, request, abort
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
-from flaskblog.DummyData import posts
 from flask_login import login_user, logout_user, current_user, login_required
 
-    
+
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template("home.html", posts=posts)
     
 @app.route("/about")
@@ -47,7 +50,81 @@ def logout():
     flash(f"Successfully logged out!", "success")
     return redirect(url_for('home'))
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + ext
+    picture_path = os.path.join(app.root_path, 'static/profile_images', picture_fn)
+    resized_img = Image.open(form_picture)
+    resized_img.thumbnail((200, 200))
+    resized_img.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods=["POST", "GET"])
 @login_required
-def account():    
-    return render_template('account.html', title="Account")    
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash(f"Changes were successfully executed!", "success")
+        return redirect(url_for('account'))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    user_image = url_for('static', filename='profile_images/' + current_user.image)   
+    return render_template('account.html', title="Account", user_image=user_image,
+                            form=form)    
+    
+@app.route("/post/new", methods=["POST", "GET"])
+@login_required
+def new_post():
+    form = PostForm()    
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.post_content.data,
+                    author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post has been created!", "success")
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title="Create a post", form=form,
+                            legend="Create a post")
+    
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+    
+@app.route("/post/<int:post_id>/update", methods=["POST", "GET"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.post_content.data
+        db.session.commit()
+        flash("Your post has been updated!", "success")
+        return redirect(url_for('post', post_id=post.id))
+    form.title.data = post.title
+    form.post_content.data = post.content
+    return render_template('create_post.html', title="Update a post", form=form,
+                            legend="Update a post")    
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):    
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f"Post has been deleted!", "success")
+    return redirect(url_for('home'))
+
